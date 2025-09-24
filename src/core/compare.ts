c3b7d23126233a2ae9a0754350b3cca3e354f61f
Pavel Makeev <pavel.makeev.qubership@gmail.com>
2025-09-24 15:47:52 +0400

    fix: added gitleaks:allow in line

--- src/core/compare.ts
+++ src/core/compare.ts
@@ -238,8 +238,7 @@ const useMergeFactory = (onDiff: DiffCallback, options: InternalCompareOptions):
 
     const beforeKey = unsafeKey ?? (isArray(beforeJso) ? +Object.keys(keyMap).pop()! : Object.keys(keyMap).pop())
     const afterKey = keyMap[beforeKey]
-    // gitleaks:allow(generic-api-key)
-    const mergeKey = isArray(mergedJso) && isNumber(beforeKey) ? beforeKey : afterKey//THIS IS VERY FRAGILE. Cause this logic duplicate this line mergedJsoValue[keyInMerge] = afterValue[keyInAfter]
+    const mergeKey = isArray(mergedJso) && isNumber(beforeKey) ? beforeKey : afterKey //gitleaks:allow //THIS IS VERY FRAGILE. Cause this logic duplicate this line mergedJsoValue[keyInMerge] = afterValue[keyInAfter]
 
     // skip if node was removed
     if (!(beforeKey in keyMap)) {
